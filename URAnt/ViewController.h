20247d041071d30181a41578f1b35ffca024660e
superweiyan <345917048@qq.com>
2019-05-14 23:21:14 +0800

    add files

---
+++ URAnt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  URAnt
+//
+//  Created by linweiyan on 2019/5/14.
+//  Copyright © 2019年 linweiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ URAntTests/URAntTests.m
@@ -0,0 +1,37 @@
+//
+//  URAntTests.m
+//  URAntTests
+//
+//  Created by linweiyan on 2019/5/14.
+//  Copyright © 2019年 linweiyan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface URAntTests : XCTestCase
+
+@end
+
+@implementation URAntTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
